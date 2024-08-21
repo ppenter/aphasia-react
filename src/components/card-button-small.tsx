6dc116637cc2b135bb307176f34fd59b84c8617d
ppenter <jarupak.sri@gmail.com>
2024-08-21 15:21:14 +0700

    Update card-button-small.tsx

--- src/components/card-button-small.tsx
+++ src/components/card-button-small.tsx
@@ -12,11 +12,11 @@ export const CardButtonSmall = ({
 
   const inner = () => {
     return (
-      <div className="relative flex flex-col items-center w-full gap-0 p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 aspect-square h-[280px] justify-between">
+      <div className="relative flex flex-col items-center w-full gap-0 p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 aspect-square h-[320px] justify-between">
         {/* Full width image */}
         <img
           src={img}
-          className="object-cover w-full rounded-t-lg max-w-[120px]"
+          className="object-cover w-full rounded-t-lg max-w-[240px]"
           onError={({ currentTarget }) => {
             currentTarget.onerror = null;
             currentTarget.src = "/images/menu/speaker.png";
